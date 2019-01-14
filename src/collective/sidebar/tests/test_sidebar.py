1c7f886f16099a54d74a20f9a764a924017bd0c0
Jesse Stippel <jesse.stippel@operun.de>
2019-01-14 11:25:56 +0100

    added tests for profile section

---
+++ src/collective/sidebar/tests/test_sidebar.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from collective.sidebar.testing import COLLECTIVE_SIDEBAR_FUNCTIONAL_TESTING
+from plone import api
+from plone.app.testing import login
+from plone.app.testing import logout
+from plone.app.testing import SITE_OWNER_NAME
+
+import unittest
+
+
+class TestSidebarFunctional(unittest.TestCase):
+
+    layer = COLLECTIVE_SIDEBAR_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        login(self.portal, SITE_OWNER_NAME)
+
+    def test_profile_section(self):
+        nav_data_view = api.content.get_view(
+            name='navData',
+            context=self.portal,
+            request=self.request,
+        )
+        viewlet = nav_data_view.render_viewlet
+        viewlet(context=self.portal, request=self.request)
+        self.assertIn(
+            '<div class="profile-name">admin</div>',
+            viewlet(context=self.portal, request=self.request),
+        )
+        # Login with user that has fullname property set
+        logout()
+        login(self.portal, 'mmustermann')
+        self.assertIn(
+            '<div class="profile-name">Max Mustermann</div>',
+            viewlet(context=self.portal, request=self.request),
+        )
