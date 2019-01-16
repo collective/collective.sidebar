556cb5b44f102a0a52d4359e95948f8d92744a95
Jesse Stippel <jesse.stippel@operun.de>
2019-01-16 20:35:58 +0100

    added collapse mode for editing

--- src/collective/sidebar/browser/static/sidebar.js
+++ src/collective/sidebar/browser/static/sidebar.js
@@ -7,7 +7,7 @@
     $('#portal-globalnav').prepend('<li id=\"portaltab-burger-menu\"><i class=\"glyphicon glyphicon-menu-hamburger\" /></li>');
 
     // Mouse
-    $('body').on('mousemove',function(event) {
+    $('body').on('mousemove', function(event) {
       if (event.pageX < 20) {
         $('body').attr('data-with-sidebar', 'true');
       }
@@ -25,6 +25,16 @@
     $('#portal-navigation-cover').click(function(e) {
       e.preventDefault();
       $('body').attr('data-with-sidebar', '');
+    });
+
+    // Sidebar Collapse
+
+    $('#portal-navigation #menu-collapse').click(function(e) {
+      $('.menu .menu-section').fadeOut(150, function() {
+        $('#portal-navigation').toggleClass('collapsed').one('transitionend', function() {
+          $('.menu .menu-section').fadeIn(150);
+        });
+      });
     });
 
   });
