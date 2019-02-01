d6328bd6219f4f010eedde17a61885f15ca30e8e
Jesse Stippel <jesse.stippel@operun.de>
2019-02-01 12:58:26 +0100

    Preparing release 1.0.0a4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.sidebar',
-    version='1.0.0a4.dev0',
+    version='1.0.0a4',
     description="A sidebar for Plone to consolidate toolbar and navigation.",
     long_description=long_description,
     classifiers=[
