534303960c7d0f035884771a0b3c5733281f2da8
Jesse Stippel <jesse.stippel@operun.de>
2019-07-16 16:33:20 +0200

    fix structure updater

--- src/collective/sidebar/browser/static/mockup-patterns-structureupdater.js
+++ src/collective/sidebar/browser/static/mockup-patterns-structureupdater.js
@@ -1,14 +1,30 @@
-(function ($, Base) {
+if (require === undefined) {
+  require = function (reqs, torun) {
+    'use strict';
+    return torun(window.jQuery);
+  }
+}
+
+require([
+  'jquery',
+  'pat-base',
+], function ($, Base) {
   'use strict';
-  var Pattern = Base.extend({
+
+  var SidebarUpdater = Base.extend({
     name: 'sidebar-structureupdater',
     trigger: '.template-folder_contents',
     parser: 'mockup',
     init: function () {
       $('body').on('context-info-loaded', function (e, data) {
+        var base_url = $('body').data('base-url');
+        var path = base_url + '/navData';
+        if (data.object) {
+          path = data.object.getURL + '/navData';
+        }
         $.ajax({
           type: "post",
-          url: data.object.getURL + '/navData',
+          url: path,
           data: 'render=' + 1,
           success: function (nav) {
             $('#portal-navigation').html(nav);
@@ -17,5 +33,7 @@
       }.bind(this));
     }
   });
-  return Pattern;
-})
+
+  return SidebarUpdater;
+
+});
