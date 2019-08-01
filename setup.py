2a656a3e84dd8b2769257e27599caa607b79cef5
Stefan Arnold <stefan.arnold@operun.de>
2019-08-01 13:12:49 +0200

    Back to development: 1.0.0b1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.sidebar',
-    version='1.0.0a10',
+    version='1.0.0b1.dev0',
     description="A sidebar for Plone to consolidate toolbar and navigation.",
     long_description=long_description,
     classifiers=[
