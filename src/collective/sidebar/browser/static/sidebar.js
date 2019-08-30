8613713aab781c33fb9eb2ea6fcf8347f8e885d0
Jesse Stippel <jesse.stippel@operun.de>
2019-08-30 15:35:25 +0200

    Sidebar Positioning (#70)
    * added sidebar positioning

--- src/collective/sidebar/browser/static/sidebar.js
+++ src/collective/sidebar/browser/static/sidebar.js
@@ -1,28 +1,52 @@
-(function($) {
+(function ($) {
   'use strict'; // Start of use strict
 
-  $(document).ready(function() {
+  function nearTo($element, distance, side, event) {
+    var left = $element.offset().left;
+    var loffset = left + distance;
+    var right = left + $element.width();
+    var roffset = right - distance;
+    var x = event.pageX;
+    if (side == 'left') {
+      return x < loffset;
+    };
+    if (side == 'right') {
+      return x > roffset;
+    };
+  };
 
-    // Add burger icon to navigation
+  $(document).ready(function () {
+
+    // Burger Icon
     $('#portal-globalnav').prepend('<li id=\"portaltab-burger-menu\"><i class=\"glyphicon glyphicon-menu-hamburger\" /></li>');
 
     // Mouse
-    $('body').on('mousemove',function(event) {
-      if (event.pageX < 20) {
-        $('body').attr('data-with-sidebar', 'true');
-      }
+    $('body').mousemove(function (event) {
+      var nav = $('#portal-navigation');
+      var offset = 10;
+      if (nav.hasClass('sidebar-left')) {
+        var nearby = nearTo(nav, offset, 'left', event);
+        if (nearby) {
+          $('body').attr('data-with-sidebar', 'true');
+        };
+      } else {
+        var nearby = nearTo(nav, offset, 'right', event);
+        if (nearby) {
+          $('body').attr('data-with-sidebar', 'true');
+        };
+      };
     });
 
     // Burger
 
-    $('#portaltab-burger-menu').click(function(e) {
+    $('#portaltab-burger-menu').click(function (e) {
       e.preventDefault();
       $('body').attr('data-with-sidebar', 'true');
     });
 
     // Sidebar Cover
 
-    $('#portal-navigation-cover').click(function(e) {
+    $('#portal-navigation-cover').click(function (e) {
       e.preventDefault();
       $('body').attr('data-with-sidebar', '');
     });
