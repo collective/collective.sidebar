232682b46f019aee12456e885b8e131d2de5edfc
Stefan Antonelli <stefan.antonelli@operun.de>
2023-02-09 12:08:08 +0100

    update package information

--- setup.py
+++ setup.py
@@ -20,9 +20,11 @@ setup(
     classifiers=[
         "Environment :: Web Environment",
         "Framework :: Plone",
-        "Framework :: Plone :: 5.2",
+        "Framework :: Plone :: 6.0",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
