f08d552c0c7dcc85dd29f3995e31a90031f8c8bd
Espen Moe-Nilssen <espen@medialog.no>
2025-02-25 17:21:52 +0100

    Update sidebar.py
    Fix for plone 6, remove getFolderContents

--- src/collective/sidebar/browser/sidebar.py
+++ src/collective/sidebar/browser/sidebar.py
@@ -28,11 +28,7 @@ class NavigationView(BrowserView):
 
     def __call__(self):
         return self.template()
-    
-    def getFolderContents(self, item):
-        url = item.absolute_url
-        return self.context.portal_catalog(path={'query': url, 'depth': 1})
-        
+
     def check_displayed_types(self, item):
         """
         Check settings if content type should be displayed in navigation.
@@ -115,7 +111,8 @@ class NavigationView(BrowserView):
         """
         Check if navigation will return items for folder
         """
-        items = self.getFolderContents(item)
+        itemobj = item.getObject()
+        items = itemobj.restrictedTraverse('@@contentlisting')()
         for item in items:
             if self.check_item(item):
                 return True
@@ -151,7 +148,8 @@ class NavigationView(BrowserView):
 
         # Can not remember what edgecase we catch here.
         try:
-            contents = self.getFolderContents(context)
+            contents = context.restrictedTraverse('@@contentlisting')()
+            # contents = context.getFolderContents()
         except Exception:  # noqa: 902
             pass
 
@@ -165,8 +163,8 @@ class NavigationView(BrowserView):
                 if item.is_folderish and self.contains_items(item):
                     item_type = 'link-folder'
                 data = {
-                    'title': item.Title,
-                    'title_cropped': crop(item.Title, 100),
+                    'title': item.Title(),
+                    'title_cropped': crop(item.Title(), 100),
                     'url': url,
                     'type': item_type,
                 }
@@ -176,10 +174,6 @@ class NavigationView(BrowserView):
 
 class SidebarViewlet(ViewletBase):
     index = ViewPageTemplateFile('templates/sidebar.pt')
-
-    def getFolderContents(self, item):
-        url = item.absolute_url
-        return self.context.portal_catalog(path={'query': url, 'depth': 1})
 
     def get_mouse_activated(self):
         """Pass in values to be used in JavaScript
@@ -382,20 +376,20 @@ class SidebarViewlet(ViewletBase):
             context = api.portal.get_navigation_root(context)
         contents = []
         if IFolderish.providedBy(context):
-            contents = self.getFolderContents(context)
+            contents = context.restrictedTraverse('@@contentlisting')()
         else:
             # Can not remember what edgecase we catch here.
             try:
                 parent = context.aq_parent
-                contents = self.getFolderContents(parent)
+                contents = parent.restrictedTraverse('@@contentlisting')()
             except Exception:  # noqa: 902
                 pass
         items = []
         for item in contents:
             if self.check_item(item):
                 data = {
-                    'title': item.Title,
-                    'title_cropped': crop(item.Title, 100),
+                    'title': item.Title(),
+                    'title_cropped': crop(item.Title(), 100),
                     'url': item.getURL(),
                 }
                 items.append(data)
